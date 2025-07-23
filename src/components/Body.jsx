1bdeb6c312a599fffef81df4bcc2ae170bf47d10
Prakash <mprakash4678@gmail.com>
2025-07-23 10:39:20 +0530

    add:Card

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,21 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
-
+  const [res, setRes] = useState(resList); 
 
   return (
     <div className="body">
-      <div className="btn"><button>
-        Filter</button></div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = res.filter(
+              (restaurant) => restaurant.info.avgRating > 4.5
+            );
+            setRes(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+
       <div className="res-container">
-        
-        {
-          resList.map((restaurant ) => <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
-        }
-        
+        {res.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
 };
+
 export default Body;

4e45481a9b861fd4a69aed2157412f9ec6208b73
Prakash <mprakash4678@gmail.com>
2025-07-25 20:37:42 +0530

    add:Custom hook for online status & lazy loading

--- src/utils/useOnlineStatus.jsx
+++ src/utils/useOnlineStatus.jsx
@@ -10,10 +10,10 @@ const useOnlineStatus = () => {
     window.addEventListener("online", handleOnline);
     window.addEventListener("offline", handleOffline);
 
-    // return () => {
-    //   window.removeEventListener("online", handleOnline);
-    //   window.removeEventListener("offline", handleOffline);
-    // };
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
   }, []);
 
   return isOnline;
